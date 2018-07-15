9180976478ad7c6804ce0b3ae7c1b99958941ccd
gperez8 <gregory.facyt@gmail.com>
2018-07-15 15:37:21 -0400

    Aprendi como enviar valores desde la terminal a un script de nodeJs, utilizando process.argv

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,6 +1,14 @@
 const { crearArchivo }  = require('./multiplicar/multiplicar');
 
-const base = 'sdjfkldsl';
+// Array de argumentos global en node
+const argv = process.argv;
+
+// Se toma el segudo elemento del array de argumentos
+const parametros = argv[2];
+
+// separamos el nombre de la varible enviada desde la terminal
+// de su valor
+const base = parametros.split('=')[1];
 
 crearArchivo(base)
     .then(archivo => console.log(`Archivo creado: ${ archivo }`))
