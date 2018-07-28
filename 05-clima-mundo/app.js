7b28c99ae79fdfe81567790c1c06b7f47bd1b6ca
gperez8 <gregory.facyt@gmail.com>
2018-07-27 21:58:22 -0400

    se establecio conexion con google map

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -6,4 +7,24 @@ const argv = require('yargs').options({
     }
 }).argv;
 
-console.log(argv.direccion);
+const  encodeUrl = encodeURI(argv.direccion);
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ encodeUrl }&key=AIzaSyA-HXVa2jtkGfKtIJwisxgC46RaWqC1xuI`)
+    .then( resp => {
+        // truco para ver todo lo que trae un JSON
+        // console.log(JSON.stringify(resp.data, undefined, 2));
+        // const data = resp.data.results[0];
+
+
+        if (resp.data.status !== 'ZERO_RESULTS') {
+            const data = resp.data.results[0];
+
+            console.log('----------------------------------------------------------');
+            console.log('Dirección: ', data.formatted_address);
+            console.log('Latitud: ', data.geometry.location.lat);
+            console.log('Longitud: ', data.geometry.location.lng);
+            console.log('-----------------------------------------------------------');
+        } else {
+            console.log('Direccion no valida');
+        }
+    })
+    .catch( e => console.log(e.response) )
