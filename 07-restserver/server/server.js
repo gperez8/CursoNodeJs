a5f0095998ff8c49358ea83890e6ba00706b0cb0
gperez8 <gregory.facyt@gmail.com>
2018-08-04 22:44:53 -0400

    Se creo el servidor y una serie de endpoints

---
+++ 07-restserver/server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', function (req, res) {
+  res.json('get Usuario')
+})
+
+app.post('/usuario', function (req, res) {
+  const data = req.body;
+
+  if (data.nombre === undefined) {
+    res.status(400).json({
+       ok: false,
+       msj: 'Nombre requerido'
+    })
+  } else {
+
+  }
+
+  res.json({data});
+})
+
+app.put('/usuario', function (req, res) {
+  res.json('get Usuario')
+})
+
+app.delete('/usuario', function (req, res) {
+  res.json('get Usuario')
+})
+ 
+app.listen(3000, () => {
+  console.log('Escuchando por el puerto 3000');
+});
